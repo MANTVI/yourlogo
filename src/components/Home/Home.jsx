2da67e78c133c65ba0674e791b5de5a2df6b3960
MANTVI <suresh.sc123.123@gmail.comI>
2024-08-20 11:31:05 +0530

    add call button again

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function Home() {
 
     const handleCall = () => {
-        window.location.href = 'tel:+1234567890';}
+        window.location.href = 'tel:+919084620887';}
     return (
         <div className="mx-auto w-full max-w-7xl">
             <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
